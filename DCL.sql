e94a40a3456819db00665b24b5f384c32a747592
imwkdrh <imwkdrh7@naver.com>
2024-07-23 15:50:53 +0900

    2024-07-23 15:50

--- DCL.sql
+++ DCL.sql
@@ -10,4 +10,4 @@ GRANT all ON *.* TO 'developer'@'127.0.0.1';
 -- REVOKE : 특정 사용자에게 스키마에 대한 권한을 회수하는 명령어
 -- REVOKE 권한리스트 ON 데이터베이스.테이블 FROM 유저이름@접근위치;
 
-REVOKE ALL ON *.* FROM 'developer'@'127.0.0.1';
+REVOKE ALL ON *.* FROM 'developer'@'127.0.0.1';s 
